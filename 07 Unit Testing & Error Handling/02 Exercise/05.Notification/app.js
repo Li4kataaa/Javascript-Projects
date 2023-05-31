95fac828dab1c9c7586f713a50b55ac5ffa418c0
Ilian Sharkov <62841691+Li4kataaa@users.noreply.github.com>
2023-05-31 17:58:57 +0300

    Delete 07 Unit Testing & Error Handling directory

--- 07 Unit Testing & Error Handling/02 Exercise/05.Notification/app.js
+++
@@ -1,6 +0,0 @@
-function notify (message) {
-    const notifDiv = document.getElementById(`notification`)
-    notifDiv.textContent = message
-    notifDiv.style.display = 'block'
-    notifDiv.addEventListener('click', () => notifDiv.style.display = 'none')
-}
