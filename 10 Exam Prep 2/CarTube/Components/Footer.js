a26b929b556fbf0371eee26f9752b1d21636766f
Ilian Sharkov <62841691+Li4kataaa@users.noreply.github.com>
2023-05-31 18:10:52 +0300

    Delete 10 Exam Prep 2/CarTube directory

--- 10 Exam Prep 2/CarTube/Components/Footer.js
+++
@@ -1,8 +0,0 @@
-import { html } from '../node_modules/lit-html/lit-html.js'
-
-const Footer = () => html`
-    <footer>
-        <p>&copy; All rights reserved</p>
-    </footer>`
-
-export { Footer }
